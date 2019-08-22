112e9f2d84c35e54e8ca66c0c80c8bda0a2c929d
od3n <mr.od3n@gmail.com>
2019-08-22 14:57:41 +0800

    Create a page /properties that shows all property resources

---
+++ tests/Feature/ManagePropertyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Property;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManagePropertyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_that_view_has_property_resources()
+    {
+        $this->withoutExceptionHandling();
+
+        factory(Property::class, 20)->create();
+
+        $this->get('/properties')
+             ->assertStatus(200)
+             ->assertViewHasAll(['properties' => Property::all()]);
+    }
+}
